74cb162340df1b394f85bf64d366fede3414cf5d
1316-dev <mathieu.pineau@hotmail.fr>
2026-03-13 21:14:32 +0100

    function shuffle

--- js/app.js
+++ js/app.js
@@ -10,6 +10,16 @@ const questions = questionsData.map(
                               question.reponseCorrecte)
 );
 
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }   
+    return array;
+}
+
+shuffle(questions);
+
 const quiz = new Quiz (questions,0);
 quiz.afficherQuestion(quiz.questions[0]); 
 
--- js/donnees.js
+++ js/donnees.js
@@ -13,5 +13,10 @@ export const questionsData = [
     enonce: "En quelle années est sortie le film Astérix : Mission cléopatre ?",
     reponses: ["2000", "2001", "2002"],
     reponseCorrecte : 2
-  }
+  },
+   {
+    enonce: "De Quel couleur est la casquette de Luigi ?",
+    reponses: ["vert", "rouge", "bleu"],
+    reponseCorrecte : 0
+  },
 ];
